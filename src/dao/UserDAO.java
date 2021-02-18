f126a643473ccc627fef6fe9af11dd353f8cf14e
shoi <takuan0113@gmail.com>
2021-02-19 08:16:12 +0900

    ユーザーのDBへのアクセスプログラム作成

---
+++ src/dao/UserDAO.java
@@ -0,0 +1,36 @@
+package dao;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+public class UserDAO {
+	
+	//名前を確認する
+	public boolean checkName(String name) {
+		String url = "jdbc:mysql://localhost:3306/managerDB";
+		String user = "root";
+		String password = "access01";
+
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+
+		String sql = "SELECT name FROM users WHERE name=?";
+		try {
+			Connection connection = DriverManager.getConnection(url, user, password);
+			PreparedStatement statment = connection.prepareStatement(sql);
+			statment.setString(1, name);
+			ResultSet rs = statment.executeQuery();
+			if(rs.next()) {
+				return  false;
+			}
+		} catch(Exception e) {
+			e.getStackTrace();
+		}	
+		return true;
+	}
+}
