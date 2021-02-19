233e34182f5587099b5d13b6610e17b26f2bd42f
shoi <takuan0113@gmail.com>
2021-02-20 05:04:14 +0900

    ユーザーチェックの編集、ユーザー登録の作成

--- src/dao/UserDAO.java
+++ src/dao/UserDAO.java
@@ -4,33 +4,54 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.sql.SQLException;
 
 public class UserDAO {
 	
 	//名前を確認する
-	public boolean checkName(String name) {
+	public String checkName(String name) throws ClassNotFoundException,SQLException{
+		Connection connection = null;
 		String url = "jdbc:mysql://localhost:3306/managerDB";
 		String user = "root";
 		String password = "access01";
 
-		try {
-			Class.forName("com.mysql.jdbc.Driver");
-		} catch(Exception e) {
-			e.printStackTrace();
-		}
-
 		String sql = "SELECT name FROM users WHERE name=?";
 		try {
-			Connection connection = DriverManager.getConnection(url, user, password);
+			Class.forName("com.mysql.jdbc.Driver");
+			connection = DriverManager.getConnection(url, user, password);
 			PreparedStatement statment = connection.prepareStatement(sql);
 			statment.setString(1, name);
 			ResultSet rs = statment.executeQuery();
 			if(rs.next()) {
-				return  false;
+				return  "その名前はすでに使われています";
 			}
-		} catch(Exception e) {
-			e.getStackTrace();
+		} finally { 
+			if (connection != null) {
+				connection.close();
+			}
+		}
+		return "";
+	}
+	
+	//ユーザーを登録する
+	public void createUser(String name, String userPass) throws ClassNotFoundException, SQLException{
+		Connection connection = null;
+		String url = "jdbc:mysql://localhost:3306/managerDB";
+		String user = "root";
+		String password = "access01";
+		
+		String sql = "INSERT INTO users (name, password) VALUES (?, ?)";
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+			connection = DriverManager.getConnection(url, user, password);
+			PreparedStatement statment = connection.prepareStatement(sql);
+			statment.setString(1, name);
+			statment.setString(2, userPass);
+			statment.executeUpdate();
+		} finally { 
+			if (connection != null) {
+				connection.close();
+			}
 		}	
-		return true;
 	}
 }
