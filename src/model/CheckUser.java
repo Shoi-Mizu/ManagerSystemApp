ae37e70b40414e0d1e1c7132e2f9591088bbc0f6
shoi <takuan0113@gmail.com>
2021-02-20 05:03:32 +0900

    ユーザーチェックのロジック作成

--- src/model/CheckUser.java
+++ src/model/CheckUser.java
@@ -1,5 +1,7 @@
 package model;
 
+import java.sql.SQLException;
+
 import dao.UserDAO;
 
 public class CheckUser {
@@ -11,9 +13,28 @@ public class CheckUser {
 		this.userPass = userPass;
 	}
 	
-	public boolean userCheck() {
-		UserDAO uDao = new UserDAO();
-		uDao.checkName(name);
-		return true;
+	public String userCheck() {
+		String errMessage = "";
+		//空欄チェック
+		if(name == null) {
+			return "名前を入力してください";
+		} else if(name.isEmpty()) {
+			return "名前を入力してください";			
+		}
+		if(userPass == null) {
+			return "パスワードを入力してください";
+		} else if(userPass.isEmpty()) {
+			return "パスワードを入力してください";			
+		}
+		
+		//重複チェック
+		try {
+			UserDAO uDao = new UserDAO();
+			errMessage = uDao.checkName(name);
+		} catch (ClassNotFoundException | SQLException e) {
+			e.printStackTrace();
+			return e.getMessage();
+		}
+		return errMessage;
 	}
 }
