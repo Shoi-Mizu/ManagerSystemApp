bf34387672dc399e9f78ebac83a8001fc2749ab7
shoi <takuan0113@gmail.com>
2021-02-21 18:18:30 +0900

    idの追加＆パスワード確認メソッド追加＆ユーザー名取得メソッド追加

--- src/dao/UserDAO.java
+++ src/dao/UserDAO.java
@@ -8,45 +8,72 @@ import java.sql.SQLException;
 
 public class UserDAO {
 	
-	//名前を確認する
-	public String checkName(String name) throws ClassNotFoundException,SQLException{
+	//IDを確認する(登録してあればtrue)
+	public boolean checkId(String id) throws ClassNotFoundException,SQLException{
 		Connection connection = null;
 		String url = "jdbc:mysql://localhost:3306/managerDB";
 		String user = "root";
 		String password = "access01";
 
-		String sql = "SELECT name FROM users WHERE name=?";
+		String sql = "SELECT id FROM users WHERE id=?";
 		try {
 			Class.forName("com.mysql.jdbc.Driver");
 			connection = DriverManager.getConnection(url, user, password);
 			PreparedStatement statment = connection.prepareStatement(sql);
-			statment.setString(1, name);
+			statment.setString(1, id);
 			ResultSet rs = statment.executeQuery();
-			if(rs.next()) {
-				return  "その名前はすでに使われています";
+			if(!rs.next()) {
+				return false;
 			}
 		} finally { 
 			if (connection != null) {
 				connection.close();
 			}
 		}
-		return "";
+		return true;
 	}
 	
-	//ユーザーを登録する
-	public void createUser(String name, String userPass) throws ClassNotFoundException, SQLException{
+	//パスワードを確認する(登録してあればtrue)
+	public boolean checkPassword(String userPass) throws ClassNotFoundException,SQLException{
 		Connection connection = null;
 		String url = "jdbc:mysql://localhost:3306/managerDB";
 		String user = "root";
 		String password = "access01";
 		
-		String sql = "INSERT INTO users (name, password) VALUES (?, ?)";
+		String sql = "SELECT password FROM users WHERE password=?";
 		try {
 			Class.forName("com.mysql.jdbc.Driver");
 			connection = DriverManager.getConnection(url, user, password);
 			PreparedStatement statment = connection.prepareStatement(sql);
-			statment.setString(1, name);
-			statment.setString(2, userPass);
+			statment.setString(1, userPass);
+			ResultSet rs = statment.executeQuery();
+			if(!rs.next()) {
+				return false;
+			}
+		} finally { 
+			if (connection != null) {
+				connection.close();
+			}
+		}
+		return true;
+	}
+	
+	
+	//ユーザーを登録する
+	public void createUser(String id, String name, String userPass) throws ClassNotFoundException, SQLException{
+		Connection connection = null;
+		String url = "jdbc:mysql://localhost:3306/managerDB";
+		String user = "root";
+		String password = "access01";
+		
+		String sql = "INSERT INTO users (id, name, password) VALUES (?, ?, ?)";
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+			connection = DriverManager.getConnection(url, user, password);
+			PreparedStatement statment = connection.prepareStatement(sql);
+			statment.setString(1, id);
+			statment.setString(2, name);
+			statment.setString(3, userPass);
 			statment.executeUpdate();
 		} finally { 
 			if (connection != null) {
@@ -54,4 +81,31 @@ public class UserDAO {
 			}
 		}	
 	}
+	
+	//ユーザー名を取得する
+	public String getUserName(String id) throws ClassNotFoundException, SQLException{
+		String name = "";
+		Connection connection = null;
+		String url = "jdbc:mysql://localhost:3306/managerDB";
+		String user = "root";
+		String password = "access01";
+		
+		String sql = "SELECT * FROM users WHERE id=?";
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+			connection = DriverManager.getConnection(url, user, password);
+			PreparedStatement statment = connection.prepareStatement(sql);
+			statment.setString(1, id);
+			ResultSet rs = statment.executeQuery();
+			if(rs.next()) {				
+				name = rs.getString("name");
+			}
+		} finally { 
+			if (connection != null) {
+				connection.close();
+			}
+		}	
+		return name;
+	}
+	
 }
