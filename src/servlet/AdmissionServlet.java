9d6e33db6e1ad4fb9e58284b781725ac0a99a1c8
shoi <takuan0113@gmail.com>
2021-02-26 20:54:22 +0900

    セッション情報の有無を追加

--- src/servlet/AdmissionServlet.java
+++ src/servlet/AdmissionServlet.java
@@ -35,34 +35,42 @@ public class AdmissionServlet extends HttpServlet {
 		String forward = "WEB-INF/jsp/admission.jsp";
 		RequestDispatcher dispatcher = request.getRequestDispatcher(forward);
 		
-		TimeBean tBean = new TimeBean();
-		try {			
-			Admission admission = new Admission();
-			isAdmission = admission.canAdmission(id, date, tBean); //出勤する
-		} catch (ClassNotFoundException | SQLException e) {
-			e.printStackTrace();
-			request.setAttribute("errMessage", "システムエラーが発生しました");
-			request.setAttribute("logind", logind);
-			forward = "WEB-INF/jsp/home.jsp";
+		//セッション情報が空ならログアウト(タイムアウト時など)
+		if(user == null) {
+			forward = "WEB-INF/jsp/logout.jsp";
 			dispatcher = request.getRequestDispatcher(forward);
 			dispatcher.forward(request, response);
+		} else {
+			TimeBean tBean = new TimeBean();
+			try {			
+				Admission admission = new Admission();
+				isAdmission = admission.canAdmission(id, date, tBean); //出勤する
+			} catch (ClassNotFoundException | SQLException e) {
+				e.printStackTrace();
+				request.setAttribute("errMessage", "システムエラーが発生しました");
+				request.setAttribute("logind", logind);
+				forward = "WEB-INF/jsp/home.jsp";
+				dispatcher = request.getRequestDispatcher(forward);
+				dispatcher.forward(request, response);
+			}
+			
+			//出勤登録できたら出勤完了画面に遷移
+			if(isAdmission) {
+				session.setAttribute("tBean", tBean);
+				request.setAttribute("logind", logind);
+				dispatcher.forward(request, response);
+			//既に登録済みであったらホーム画面にエラーメッセージ表示
+			} else {
+				session.setAttribute("tBean", tBean);
+				request.setAttribute("errMessage", "本日は既に出勤しています");
+				request.setAttribute("logind", logind);
+				forward = "WEB-INF/jsp/home.jsp";
+				dispatcher = request.getRequestDispatcher(forward);
+				dispatcher.forward(request, response);
+				
+			}
 		}
 		
-		//出勤登録できたら出勤完了画面に遷移
-		if(isAdmission) {
-			session.setAttribute("tBean", tBean);
-			request.setAttribute("logind", logind);
-			dispatcher.forward(request, response);
-		//既に登録済みであったらホーム画面にエラーメッセージ表示
-		} else {
-			session.setAttribute("tBean", tBean);
-			request.setAttribute("errMessage", "本日は既に出勤しています");
-			request.setAttribute("logind", logind);
-			forward = "WEB-INF/jsp/home.jsp";
-			dispatcher = request.getRequestDispatcher(forward);
-			dispatcher.forward(request, response);
-			
-		}
 	}
 
 }
